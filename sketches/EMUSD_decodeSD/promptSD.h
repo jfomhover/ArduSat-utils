af66ce87ed8f7f7ff5ba81819d577c1fc3d30834
J.F. Omhover <jf.omhover@gmail.com>
2014-01-30 18:58:42 +0100

    generic decoder for SAT_DataLib
    prompts via Serial for a filename, decodes it using SAT_DataLib

---
+++ sketches/EMUSD_decodeSD/promptSD.h
@@ -0,0 +1,67 @@
+
+/*
+void printDirectory(File dir, int numTabs) {
+   while(true) {
+     
+     File entry =  dir.openNextFile();
+     if (! entry) {
+       // no more files
+       //Serial.println("**nomorefiles**");
+       break;
+     }
+     for (uint8_t i=0; i<numTabs; i++) {
+       Serial.print('\t');
+     }
+     Serial.print(entry.name());
+     if (entry.isDirectory()) {
+       Serial.println("/");
+//       printDirectory(entry, numTabs+1);
+     } else {
+       // files have sizes, directories do not
+       Serial.print("\t\t");
+       Serial.println(entry.size(), DEC);
+     }
+     entry.close();
+   }
+};
+*/
+
+boolean isFileChar(char c) {
+  if (c == '.')
+    return(true);
+  if ((c >= 'a') && (c <= 'z'))
+    return(true);
+  if ((c >= 'A') && (c <= 'Z'))
+    return(true);
+  return(false);
+};
+
+char filename[16];
+
+char * promptFile() {
+//  File root = SD.open("/");
+  
+//  printDirectory(root, 0);
+//  root.close();
+
+  Serial.println("type the name of the file you want to open");
+  Serial.print("> ");
+  
+  for (int i=0; i<16; i++)
+    filename[i] = 0x00;
+  int t_count = 0;
+  
+  while (true) {
+    if (Serial.available() > 0) {
+      filename[t_count] = Serial.read();
+      if ((!isFileChar(filename[t_count])) || (t_count >= 15)) {
+        delay(50);
+        while(Serial.available() > 0) { Serial.read(); };  // purge the input channel
+        filename[t_count] = '\0';
+        return(filename);
+      }
+      t_count++;
+    }
+  }
+};
+
